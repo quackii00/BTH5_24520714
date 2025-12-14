71ef2ff742f61665ef5421852b7a6476fdbcccc2
Nguyen Thi Ngoc Huyen <hinduck3206@gmail.com>
2025-12-15 00:31:03 +0700

    demo bài 9

---
+++ Bai09/Bai09/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Bai09
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxTest = new System.Windows.Forms.ComboBox();
+            this.panelDrawing = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // comboBoxTest
+            // 
+            this.comboBoxTest.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxTest.FormattingEnabled = true;
+            this.comboBoxTest.Items.AddRange(new object[] {
+            "Circle",
+            "Square",
+            "Ellipse",
+            "Pie",
+            "Filled Circle",
+            "Filled Square",
+            "Filled Ellipse",
+            "Filled Pie"});
+            this.comboBoxTest.Location = new System.Drawing.Point(38, 45);
+            this.comboBoxTest.Name = "comboBoxTest";
+            this.comboBoxTest.Size = new System.Drawing.Size(363, 49);
+            this.comboBoxTest.TabIndex = 0;
+            this.comboBoxTest.SelectionChangeCommitted += new System.EventHandler(this.ShapeChanged);
+            // 
+            // panelDrawing
+            // 
+            this.panelDrawing.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelDrawing.Location = new System.Drawing.Point(38, 131);
+            this.panelDrawing.Name = "panelDrawing";
+            this.panelDrawing.Size = new System.Drawing.Size(830, 454);
+            this.panelDrawing.TabIndex = 1;
+            this.panelDrawing.Paint += new System.Windows.Forms.PaintEventHandler(this.PaintShape);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 613);
+            this.Controls.Add(this.panelDrawing);
+            this.Controls.Add(this.comboBoxTest);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "ComboBoxTest";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxTest;
+        private System.Windows.Forms.Panel panelDrawing;
+    }
+}
+
---
+++ Bai09/Bai09/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai09
+{
+    public partial class Form1 : Form
+    {
+        string shape;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+        private void PaintShape(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.DeepPink, 2);
+            Brush brush = new SolidBrush(Color.Pink);
+            int x = panelDrawing.ClientSize.Width / 2;
+            int y = panelDrawing.ClientSize.Height / 2;
+            int size = 100;
+            if (shape == "Circle")
+            {
+                g.DrawEllipse(pen, x - size, y - size, size * 2, size * 2);
+            }
+            else if (shape == "Ellipse")
+            {
+                g.DrawEllipse(pen, x - (int)Math.Round(size * 1.5), y - size, size * 3, size * 2);
+            }
+            else if (shape == "Square")
+            {
+                g.DrawRectangle(pen, x - size, y - size, size * 2, size * 2);
+            }
+            else if (shape == "Pie")
+            {
+                g.DrawPie(pen, x - (int)Math.Round(size * 1.5), (int)Math.Round(y * 1.2) - size, size * 3, size * 3, 225, 90);
+            }
+            else if (shape == "Filled Square")
+            {
+                g.FillRectangle(brush, x - size, y - size, size * 2, size * 2);
+            }
+            else if (shape == "Filled Circle")
+            {
+                g.FillEllipse(brush, x - size, y - size, size * 2, size * 2);
+            }
+            else if (shape == "Filled Ellipse")
+            {
+                g.FillEllipse(brush, x - (int)Math.Round(size * 1.5), y - size, size * 3, size * 2);
+            }
+            else if (shape == "Filled Pie")
+            {
+                g.FillPie(brush, x - (int)Math.Round(size * 1.5), (int)Math.Round(y * 1.2) - size, size * 3, size * 3, 225, 90);
+            }
+           
+               
+
+        }
+
+        private void ShapeChanged(object sender, EventArgs e)
+        {
+            shape = comboBoxTest.SelectedItem.ToString();
+            panelDrawing.Invalidate();
+        }
+    }
+}
