4337e35e7ab636cff4eedd7718ec8c3b6491a975
Nguyen Thi Ngoc Huyen <hinduck3206@gmail.com>
2025-12-20 02:31:23 +0700

    demo bai 10

---
+++ Bai10/Bai10/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Bai10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cboBoxEndCap = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cboBoxStartCap = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cboBoxDashCap = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboBoxLine = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboBoxWidth = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboBoxDashStyle = new System.Windows.Forms.ComboBox();
+            this.panelDrawing = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.FloralWhite;
+            this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panelDrawing);
+            this.splitContainer1.Size = new System.Drawing.Size(1193, 687);
+            this.splitContainer1.SplitterDistance = 503;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.35763F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 62.64237F));
+            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.cboBoxEndCap, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.cboBoxStartCap, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.cboBoxDashCap, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.cboBoxLine, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.cboBoxWidth, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.cboBoxDashStyle, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(24, 44);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(453, 640);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(3, 530);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(163, 110);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "End Cap :";
+            // 
+            // cboBoxEndCap
+            // 
+            this.cboBoxEndCap.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cboBoxEndCap.FormattingEnabled = true;
+            this.cboBoxEndCap.Location = new System.Drawing.Point(172, 533);
+            this.cboBoxEndCap.Name = "cboBoxEndCap";
+            this.cboBoxEndCap.Size = new System.Drawing.Size(278, 33);
+            this.cboBoxEndCap.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 424);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(163, 106);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Start Cap :";
+            // 
+            // cboBoxStartCap
+            // 
+            this.cboBoxStartCap.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cboBoxStartCap.FormattingEnabled = true;
+            this.cboBoxStartCap.Location = new System.Drawing.Point(172, 427);
+            this.cboBoxStartCap.Name = "cboBoxStartCap";
+            this.cboBoxStartCap.Size = new System.Drawing.Size(278, 33);
+            this.cboBoxStartCap.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 318);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 106);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Dash Cap :";
+            // 
+            // cboBoxDashCap
+            // 
+            this.cboBoxDashCap.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cboBoxDashCap.FormattingEnabled = true;
+            this.cboBoxDashCap.Location = new System.Drawing.Point(172, 321);
+            this.cboBoxDashCap.Name = "cboBoxDashCap";
+            this.cboBoxDashCap.Size = new System.Drawing.Size(278, 33);
+            this.cboBoxDashCap.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 106);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Line Join :";
+            // 
+            // cboBoxLine
+            // 
+            this.cboBoxLine.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cboBoxLine.FormattingEnabled = true;
+            this.cboBoxLine.Location = new System.Drawing.Point(172, 215);
+            this.cboBoxLine.Name = "cboBoxLine";
+            this.cboBoxLine.Size = new System.Drawing.Size(278, 33);
+            this.cboBoxLine.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 106);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Width :";
+            // 
+            // cboBoxWidth
+            // 
+            this.cboBoxWidth.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cboBoxWidth.FormattingEnabled = true;
+            this.cboBoxWidth.Location = new System.Drawing.Point(172, 109);
+            this.cboBoxWidth.Name = "cboBoxWidth";
+            this.cboBoxWidth.Size = new System.Drawing.Size(278, 33);
+            this.cboBoxWidth.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 106);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dash Style :";
+            // 
+            // cboBoxDashStyle
+            // 
+            this.cboBoxDashStyle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cboBoxDashStyle.FormattingEnabled = true;
+            this.cboBoxDashStyle.Location = new System.Drawing.Point(172, 3);
+            this.cboBoxDashStyle.Name = "cboBoxDashStyle";
+            this.cboBoxDashStyle.Size = new System.Drawing.Size(278, 33);
+            this.cboBoxDashStyle.TabIndex = 1;
+            // 
+            // panelDrawing
+            // 
+            this.panelDrawing.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panelDrawing.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelDrawing.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDrawing.Location = new System.Drawing.Point(0, 0);
+            this.panelDrawing.Name = "panelDrawing";
+            this.panelDrawing.Size = new System.Drawing.Size(686, 687);
+            this.panelDrawing.TabIndex = 0;
+            this.panelDrawing.Paint += new System.Windows.Forms.PaintEventHandler(this.PaintAction);
+            this.panelDrawing.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseStartPaint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1193, 687);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pen Demo";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cboBoxEndCap;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cboBoxStartCap;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cboBoxDashCap;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboBoxLine;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboBoxWidth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboBoxDashStyle;
+        private System.Windows.Forms.Panel panelDrawing;
+    }
+}
+
---
+++ Bai10/Bai10/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Diagnostics.Eventing.Reader;
+namespace Bai10
+{
+    public partial class Form1 : Form
+    {
+        List<Point> arrowPoints = new List<Point>();
+        Bitmap canvas;
+        Graphics g;
+        public Form1()
+        {
+            InitializeComponent();
+
+            cboBoxDashStyle.DataSource = Enum.GetValues(typeof(DashStyle));
+            cboBoxDashStyle.SelectedIndex = 0;
+
+            for (int i = 1; i <= 15; i++)
+            {
+                cboBoxWidth.Items.Add(i.ToString());
+            }
+            cboBoxWidth.SelectedIndex = 4;
+
+            cboBoxLine.DataSource = Enum.GetValues(typeof(LineJoin));
+            cboBoxLine.SelectedIndex = 0;
+            cboBoxDashCap.DataSource = Enum.GetValues(typeof(DashCap));
+            cboBoxDashCap.SelectedIndex = 0;
+
+            cboBoxEndCap.DataSource = Enum.GetValues(typeof(LineCap));
+            cboBoxStartCap.DataSource = Enum.GetValues(typeof(LineCap));
+            cboBoxEndCap.SelectedIndex = 0;
+            cboBoxStartCap.SelectedIndex = 0;
+            canvas = new Bitmap(panelDrawing.Width, panelDrawing.Height);
+            g = Graphics.FromImage(canvas);
+            g.Clear(Color.White); // nền trắng
+
+            panelDrawing.Paint += PaintAction;
+        }
+
+        private void PaintAction(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(canvas, 0, 0);
+            if (arrowPoints.Count < 2) return;
+
+            DashStyle dashStyle = (DashStyle)cboBoxDashStyle.SelectedItem;
+            int Width = int.Parse(cboBoxWidth.SelectedItem.ToString());
+            LineJoin lineJoin = (LineJoin)cboBoxLine.SelectedItem;
+            DashCap dashCap = (DashCap)cboBoxDashCap.SelectedItem;
+            LineCap startCap = (LineCap)cboBoxStartCap.SelectedItem;
+            LineCap endCap = (LineCap)cboBoxEndCap.SelectedItem;
+
+            using (Pen pen = new Pen(Color.Red, Width))
+            {
+                pen.DashStyle = dashStyle;
+                pen.LineJoin = lineJoin;
+                pen.DashCap = dashCap;
+                pen.StartCap = startCap;
+                pen.EndCap = endCap;
+
+                GraphicsPath path = new GraphicsPath();
+                path.AddLines(arrowPoints.ToArray());
+                e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                e.Graphics.DrawPath(pen, path);
+            }
+        }
+        private void RePaint()
+        {
+            if (arrowPoints.Count < 2) return;
+
+            DashStyle dashStyle = (DashStyle)cboBoxDashStyle.SelectedItem;
+            int Width = int.Parse(cboBoxWidth.SelectedItem.ToString());
+            LineJoin lineJoin = (LineJoin)cboBoxLine.SelectedItem;
+            DashCap dashCap = (DashCap)cboBoxDashCap.SelectedItem;
+            LineCap startCap = (LineCap)cboBoxStartCap.SelectedItem;
+            LineCap endCap = (LineCap)cboBoxEndCap.SelectedItem;
+
+            using (Pen pen = new Pen(Color.Red, Width))
+            {
+                pen.DashStyle = dashStyle;
+                pen.LineJoin = lineJoin;
+                pen.DashCap = dashCap;
+                pen.StartCap = startCap;
+                pen.EndCap = endCap;
+
+                GraphicsPath path = new GraphicsPath();
+                path.AddLines(arrowPoints.ToArray());
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                g.DrawPath(pen, path);
+            }
+        }
+        private void MouseStartPaint(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                arrowPoints.Add(e.Location);
+                panelDrawing.Invalidate();
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                RePaint();
+                arrowPoints.Clear();
+            }
+        }
+
+
+        
+    }
+}
+
+
+
+
