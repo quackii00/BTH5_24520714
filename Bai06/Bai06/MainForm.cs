16e29001aeec365c6631dc57388387a6ddaf0b8c
Nguyen Thi Ngoc Huyen <hinduck3206@gmail.com>
2025-12-20 16:17:35 +0700

    complete bài 6

--- Bai06/Bai06/MainForm.cs
+++ Bai06/Bai06/MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 using System.Drawing.Text;
 namespace Bai06
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
             InstalledFontCollection font = new InstalledFontCollection();
